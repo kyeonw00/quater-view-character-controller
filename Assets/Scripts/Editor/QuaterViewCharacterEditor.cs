f7be6ef2f11f7e1222c3e31e74676fa1a0184e82
kyeonw00 <altair3873.luna@gmail.com>
2022-02-08 23:41:41 +0900

    add: QuaterViewCharacter debug utility added

--- Assets/Scripts/Editor/QuaterViewCharacterEditor.cs
+++ Assets/Scripts/Editor/QuaterViewCharacterEditor.cs
@@ -16,4 +16,13 @@ public class QuaterViewCharacterEditor : Editor
         Handles.color = Color.green;
         Handles.ArrowHandleCap(0, _character.transform.position, _character.transform.rotation * Quaternion.LookRotation(_character.Velocity), 2f, EventType.Repaint);
     }
+
+    [DrawGizmo(GizmoType.Active | GizmoType.Selected)]
+    static void DrawColliderBottomPointGizmos(QuaterViewCharacter src, GizmoType gizmoType)
+    {
+        Vector3 bottomPoint = src.transform.position + src.collider.center + Vector3.down * (src.collider.height / 2 - src.collider.radius + 0.015f);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(bottomPoint, src.collider.radius);
+    }
 }
--- Assets/Scripts/QuaterViewCharacter.cs
+++ Assets/Scripts/QuaterViewCharacter.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class QuaterViewCharacter : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+    public CapsuleCollider collider;
+    public float gravity;
     public float walkSpeed;
     public float runSpeed;
     public float acceleration;
+    public LayerMask obstacleLayers;
 
     private Vector3 _directionalInput = new Vector3();
     private Vector3 _velocity;
     private Vector3 _dampVelocity;
 
     public Vector3 Velocity => _velocity;
+
+    public void Reset()
+    {
+        TryGetComponent(out rigidbody);
+        TryGetComponent(out collider);
+
+        gravity = 0;
+        walkSpeed = 0;
+        runSpeed = 0;
+        acceleration = 0;
+        obstacleLayers = 0;
+    }
 
     public void Update()
     {
@@ -35,6 +49,14 @@ public class QuaterViewCharacter : MonoBehaviour
     {
         Vector3 targetVelocity = _directionalInput * walkSpeed;
         _velocity = Vector3.SmoothDamp(_velocity, targetVelocity, ref _dampVelocity, acceleration);
+    }
+
+    public void CheckCollisions()
+    {
+    }
+
+    public void CheckIfGrounded()
+    {
     }
 
     public void Move(float deltaTime)
