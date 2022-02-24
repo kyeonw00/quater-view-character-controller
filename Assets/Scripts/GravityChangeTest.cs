5457388fa44950f3ffde345fb1960fe94e58b17f
kyeonw00 <altair3873.luna@gmail.com>
2022-02-24 23:18:23 +0900

    add: Sphere friction test added

--- Assets/Scripts/GravityChangeTest.cs
+++ Assets/Scripts/GravityChangeTest.cs
@@ -17,7 +17,7 @@ public class GravityChangeTest : MonoBehaviour
     {
         _velocity = moveDirection * speed * Time.deltaTime;
 
-        float raycastLength = applyRaycastLength ? this.raycastLength : 0;
+        float raycastLength = applyRaycastLength ? _velocity.magnitude : 0;
         _collisionHitCount = Physics.SphereCastNonAlloc(transform.position, sphereRadius, moveDirection, _collisionHits, raycastLength, collisionMask);
 
         if (_collisionHitCount > 0)
@@ -25,6 +25,7 @@ public class GravityChangeTest : MonoBehaviour
             Debug.Log("Hit something");
             Vector3 adjustVelocity = _collisionHits[0].normal * _velocity.sqrMagnitude;
             _velocity.x += adjustVelocity.x;
+            _velocity.y = _collisionHits[0].distance * -1;
             _velocity.z += adjustVelocity.z;
         }
 
@@ -42,7 +43,7 @@ public class GravityChangeTest : MonoBehaviour
         for (int i = 0; i < _collisionHitCount; i++)
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawLine(_collisionHits[i].point, _collisionHits[i].point + _collisionHits[i].normal);
+            Gizmos.DrawLine(_collisionHits[i].point, _collisionHits[i].point + _collisionHits[i].normal * _collisionHits[i].distance);
             Gizmos.DrawWireSphere(_collisionHits[i].point, 0.15f);
         }
     }
